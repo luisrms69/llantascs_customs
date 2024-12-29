c5a58ccdda8eceacaf982066c0a98d6115e75749
lenovo-coatza <it@buzola.mx>
2024-12-29 18:37:50 +0000

    clean: comisiones
    Se limpia api.py de codigo comentado y sobrante

--- llantascs_customs/llantascs_customs/api.py
+++ llantascs_customs/llantascs_customs/api.py
@@ -25,11 +25,9 @@ def get_costo_ventas_si(sales_invoice_id):
         'voucher_type' : 'Sales Invoice',
         'voucher_no' : sales_invoice_id,
         'account': ['in', cogs_accounts]},
-        # 'account': ['in', ['501-000-001 - Costo de Llantas - LLCS', '401-000-001 - Ventas Nacionales - LLCS' ]]},
         pluck = 'name'
     )
 
-    # frappe.msgprint(sales_invoice_id)
     for entry in gl_entries_invoice:
         cogs += frappe.db.get_value('GL Entry', entry, 'debit')
 
@@ -38,83 +36,34 @@ def get_costo_ventas_si(sales_invoice_id):
 def get_costo_ventas_dn(sales_invoice_id):
     # GL entries for Delivery Note cases
     cogs = 0
-    # delivery_note_id = get_delivery_note_for_sales_invoice(sales_invoice_id)
-    # delivery_note_items = frappe.db.get_list('Delivery Note Item',
-    # filters = {
-    #     'against_sales_invoice' : sales_invoice_id
-    # },
-    # {'fields' : ['parent','name','qty','grant_commission','si_detail']})
-    # as_list = True)
-    # pluck = 'name')
-
-    # meta = frappe.get_meta('Delivery Note Item')
-
     dn_items_list = frappe.db.get_list(
             "Delivery Note Item",
-            # fields=[
-            #     "name",
-            #     "parent",
-            # ],
             filters={'against_sales_invoice' : sales_invoice_id},
             pluck = 'name'
-            # as_list = True
         )
 
     for dn_item in dn_items_list:
-
-    # dn_item_doc = frappe.get_doc('Delivery Note Item', dn_ans[0])
         variables = frappe.db.get_value('Delivery Note Item', dn_item,['qty','grant_commission','incoming_rate'])
         cogs += variables[0]*variables[1]*variables[2]
 
-
-
     return cogs
 
-# def get_delivery_note(dn_item):
-#     pass
-
-
-
-# def get_cogs_delivery_notes(delivery_note_items):
-#     for dn_item in delivery_note_items:
-#         dn = get_delivery_note(dn_item)
-
-
-
+# fix: no se ocupa, unicamente en pruebas, borrar
 @frappe.whitelist()
 def get_delivery_note_for_sales_invoice(sales_invoice_id):
-    # delivery_note_items = frappe.db.get_list('Delivery Note Item',
-    # filters = {
-    #     'against_sales_invoice' : sales_invoice_id
-    # },
-    # {'fields' : ['parent','name','qty','grant_commission','si_detail']})
-    # as_list = True)
-    # pluck = 'name')
-
-    # meta = frappe.get_meta('Delivery Note Item')
 
     dn_items_list = frappe.db.get_list(
             "Delivery Note Item",
-            # fields=[
-            #     "name",
-            #     "parent",
-            # ],
             filters={'against_sales_invoice' : sales_invoice_id},
             pluck = 'name'
-            # as_list = True
         )
 
     for dn_item in dn_items_list:
         cogs = 0
-    # dn_item_doc = frappe.get_doc('Delivery Note Item', dn_ans[0])
         variables = frappe.db.get_value('Delivery Note Item', dn_item,['qty','grant_commission','incoming_rate'])
         cogs += variables[0]*variables[1]*variables[2]
 
-
-
     return variables, cogs
-
-
 
 @frappe.whitelist()
 def get_sales_invoices(fecha_inicial,fecha_final):
