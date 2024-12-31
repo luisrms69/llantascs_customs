51c5b4a2c2c6c3f4e1679e50f50c68f3b4bdc601
lenovo-coatza <it@buzola.mx>
2024-12-31 04:58:07 +0000

    feat: Settings comisiones
    Se crea el doctype para los settings de comisiones, por lo pronto solo el porcentaje

---
+++ llantascs_customs/llantascs_customs/doctype/comisiones_settings/test_comisiones_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Consultoria en Negocios y Aplicaciones and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestComisionesSettings(FrappeTestCase):
+	pass
